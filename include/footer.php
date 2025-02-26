3e4000d0be28c0b972d32a8071da86634e27028b
joseph skokan <joeskokan20@gmail.com>
2025-02-26 10:38:31 -0500

    initial

---
+++ include/footer.php
@@ -0,0 +1,8 @@
+<?php
+$current_year = date('Y');
+
+?>
+
+<footer>
+    <p>&copy; <?= $current_year ?> Thai Food Yummy Restaurant. All rights reserved.</p>
+</footer>
---
+++ include/header.php
@@ -0,0 +1,60 @@
+<?php
+$restaurant_name = "🍜 Thai Food Yum Restaurant";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $restaurant_name ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/static/app.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: transparent;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/main.php"><?= $restaurant_name ?></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/main.php">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Our Menu
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="/specials.php">Our Specials</a></li>
+                                <li><a class="dropdown-item" href="/order.php">Full Menu</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/cart.php">Cart</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/about.php">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/contact.php">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <!-- pages information -->
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-<correct integrity hash>" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-<correct integrity hash>" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-<correct integrity hash>" crossorigin="anonymous"></script>
+</body>
+
+</html>
