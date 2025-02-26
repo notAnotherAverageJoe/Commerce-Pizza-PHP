2a8b7cbecb1602ec1c4911f372602b6a7e0b7149
joseph skokan <joeskokan20@gmail.com>
2025-02-26 10:54:59 -0500

    added route

--- include/header.php
+++ include/header.php
@@ -1,5 +1,5 @@
 <?php
-$restaurant_name = "🍜 Thai Food Yum Restaurant";
+$restaurant_name = "🍕 Cheesey Does It 🧀";
 ?>
 
 <!DOCTYPE html>
@@ -17,14 +17,14 @@ $restaurant_name = "🍜 Thai Food Yum Restaurant";
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: transparent;">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/main.php"><?= $restaurant_name ?></a>
+                <a class="navbar-brand" href="/"><?= $restaurant_name ?></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/main.php">Home</a>
+                            <a class="nav-link active" aria-current="page" href="/index.php">Home</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
